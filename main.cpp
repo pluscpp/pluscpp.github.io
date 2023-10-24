546ce374537baed4aa04f3dbf810e6f1b62c41c6
Marcos Oliveira <marcos@terminalroot.com.br>
2023-10-24 11:53:44 -0300

    Oi

--- main.cpp
+++ main.cpp
@@ -21,3 +21,9 @@ int main(){
 // emcmake cmake -B web .
 // cd web && make
 // emrun index.html
+// OK: ssh-keygen -t rsa -b 4096 -n /home/marcos/.ssh/pluscpp
+// eval `ssh-agent -s`
+// ssh-add ${HOME}/.ssh/pluscpp
+// git add .
+// git commit -m "Files"
+// git push origin main
