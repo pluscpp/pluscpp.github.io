44f1764e04cadda0c2478a59b6443af0a5a3c807
Marcos Oliveira <marcos@terminalroot.com.br>
2023-10-24 12:01:19 -0300

    Files

---
+++ deploy.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(int argc, char **argv){
+  std::system("mv ~/.netrc ~/.netrcBKP");
+  std::system("eval `ssh-agent -s`");
+  std::system("ssh-add ~/.ssh/pluscpp");
+  std::system("git add .");
+  std::system("git commit -m 'Files'");
+  std::system("git push origin main");
+  std::system("mv ~/.netrcBKP ~/.netrc");
+  std::system("kill -9 $(pidof ssh-agent)");
+  return 0;
+}
+
